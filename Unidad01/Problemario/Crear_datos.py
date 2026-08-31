c3a5a9ef33b1aafff428807c15ee04b7ae5273ba
brandonmartinezalexis0912-lang <brandonmartinezalexis0912@gmail.com>
2026-08-31 13:59:13 -0600

    Create Crear_datos.py

---
+++ Unidad01/Problemario/Crear_datos.py
@@ -0,0 +1,5 @@
+import pandas as pd 
+
+link_csv = "https://raw.githubusercontent.com/brandonmartinezalexis0912-lang/EstadisticaInferencial2026A/refs/heads/main/Unidad01/Problemario/Datos_problema_01.csv "
+df = pd.read_csv(link_csv)
+df
