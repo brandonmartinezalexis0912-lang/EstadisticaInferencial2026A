a0b6398ec1091d12e87f2713a534d580ba23af81
brandonmartinezalexis0912-lang <brandonmartinezalexis0912@gmail.com>
2026-09-08 13:21:27 -0600

    Delete Unidad01/codigos/Crear_datos.py

--- Unidad01/codigos/Crear_datos.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd 
-
-link_csv = "https://raw.githubusercontent.com/brandonmartinezalexis0912-lang/EstadisticaInferencial2026A/refs/heads/main/Unidad01/Problemario/Datos_problema_01.csv "
-df = pd.read_csv(link_csv)
-df
